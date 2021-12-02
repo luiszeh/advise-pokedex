80127b276f63faa523c4d33cac0a2a80242e065d
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-02 02:22:36 -0300

    Add first texts and first hr

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import '../styles/Content.css';
+
+export default function Content() {
+    return (
+        <div className="Content">
+            <div className="Content-left">
+                
+            </div>
+            <div className="Content-right">
+                <h1>Fase teste prático Front_end</h1>
+                <p>pokebusca</p>
+                <hr />
+            </div>
+        </div>
+    )
+}
