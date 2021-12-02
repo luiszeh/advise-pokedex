e152cbbb779eca8926940e1d3d898ed8b3757896
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-02 01:45:44 -0300

    Header Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import '../styles/Header.css';
+import Logo from '../images/logo-header.png';
+import { IoMdArrowDropdown } from "react-icons/io";
 
 export default function Header() {
     return (
-        <div>
-            <h2>HEADER</h2>
-        </div>
+        <header className="Header">
+            <img className="Header-logo" src={ Logo } alt="logo" />
+            <div className="Header-right">
+                <div className="Header-text">
+                    <p>Olá, Luis Fernando</p>
+                </div>
+                <div className="Header-icon">
+                    <IoMdArrowDropdown />
+                </div>
+            </div>
+        </header>
     )
 }
