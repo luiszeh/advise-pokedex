64737c0eed9ae8c98ef62008c0d33ea9d316324d
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-03 13:37:51 -0300

    Colocando as tags HR, commit na master mesmo, srry

--- src/components/PokemonThumbnail.js
+++ src/components/PokemonThumbnail.js
@@ -3,23 +3,26 @@ import '../styles/PokemonThumbnail.css';
 
 const PokemonThumbnail = ({ id, name, image }) => {
     return (
-        <div className="Thumb-container">
-            <div className="Poke-img">
-                <img src={image} alt={name} />
-            </div>
-            <div className="Poke-infos">
-                <div className="Poke-title">
-                    <div className="Poke-name">
-                        <p>{name}</p>
+        <div>
+            <div className="Thumb-container">
+                <div className="Poke-img">
+                    <img src={image} alt={name} />
+                </div>
+                <div className="Poke-infos">
+                    <div className="Poke-title">
+                        <div className="Poke-name">
+                            <p>{name}</p>
+                        </div>
+                        <div>
+                            <p>#0{id}</p>
+                        </div>
                     </div>
-                    <div>
-                        <p>#0{id}</p>
+                    <div className="Poke-feature">
+                        <p>Características do pokemon dolor sit amet, consectetur adipiscing elit. In ultricies nunc at odio tempor tincidunt. Sed sit amet dolor velit. Donec convallis tellus ut porta molestie. Suspendisse condimentum et dui in aliquam.</p>
                     </div>
                 </div>
-                <div className="Poke-feature">
-                    <p>Características do pokemon dolor sit amet, consectetur adipiscing elit. In ultricies nunc at odio tempor tincidunt. Sed sit amet dolor velit. Donec convallis tellus ut porta molestie. Suspendisse condimentum et dui in aliquam.</p>
-                </div>
             </div>
+            <hr />
         </div>
     )
 }
