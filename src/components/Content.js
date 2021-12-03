fc92f53989b041c192f328e7c599d4c1e2b1e561
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-03 13:29:55 -0300

    Criando a lista de pokemons e alinhando CSS

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,37 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Content.css';
 import { InputGroup, FormControl } from 'react-bootstrap';
+import PokemonThumbnail from './PokemonThumbnail';
+
 
 export default function Content() {
+    const [allPokemons, setAllPokemons] = useState([])
+    const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+    const getAllPokemons = async () => {
+        const res = await fetch(loadMore)
+        const data = await res.json();
+
+        setLoadMore(data.next);
+
+        function createPokemonObject(results) {
+            results.forEach( async (pokemon) => {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+                const data = await res.json();
+
+                setAllPokemons(currentList => [...currentList, data]);
+                
+            })
+        }
+        createPokemonObject(data.results);
+        await console.log(allPokemons);
+    }
+
+    useEffect(() => {
+        getAllPokemons();
+    }, []);
+
     return (
         <div className="Content">
             <div className="Content-left">
@@ -24,6 +54,17 @@ export default function Content() {
                     <p>Exibindo 20 de XXX itens</p>
                 </div>
                 <hr />
+                <div className="Pokemon-list">
+                    { allPokemons.map((pokemon, index) => 
+                        <PokemonThumbnail
+                        id={pokemon.id}
+                        name={pokemon.name}
+                        image={pokemon.sprites.other.dream_world.front_default}
+                        key={index}
+                        />
+                     )}
+                </div>
+                <button className="Button-load-more">Load more</button>
             </div>
         </div>
     )
