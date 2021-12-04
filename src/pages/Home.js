3c94e09fd51b9748bbccddc2a520286d6d663a12
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-04 19:37:42 -0300

    add footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Content from "../components/Content";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
